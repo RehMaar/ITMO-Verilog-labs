65c8ab2bd523cfccf2a974aeb8a1dda14b87e87f
RehMaar <mkdirwtf@gmail.com>
2016-12-22 12:21:27 +0300

    new test.list

--- MIPS-UART-module/hdl/wb_stage.v
+++
@@ -1,9 +0,0 @@
-`timescale 1ns / 1ps
-
-module wb_stage( input         mem_to_reg,
-                 input  [31:0] mem_out, alu_out,
-                 output [31:0] write_data );
-
-     assign write_data = mem_to_reg ? mem_out : alu_out;
-      
-endmodule
